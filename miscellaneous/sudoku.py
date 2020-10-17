65967af2b07e42862b31d21fa40d96966e006cb5
black143 <59355045+black-143@users.noreply.github.com>
2020-10-17 16:29:28 +0530

    Create sudoku.py

---
+++ miscellaneous/sudoku.py
@@ -0,0 +1,52 @@
+#Sudoku Solver using backtracking
+grid = []
+def possible(row,col,n):
+	global grid
+	for i in range(9):
+		if grid[row][i] == n or grid[i][col] == n:
+			return False
+	y0 = (row//3) * 3
+	x0 = (col//3) * 3
+	for i in range(0,3):
+		for j in range(0,3):
+			if grid[y0+i][x0+j] == n:
+				return False
+	return True
+def solve():
+	global grid
+	for row in range(9):
+		for col in range(9):
+			if grid[row][col] == 0:
+				for n in range(1,10):
+					if possible(row,col,n):
+						grid[row][col] = n
+						solve()
+						grid[row][col] = 0
+				return
+	for i in grid:
+		print(*i)
+
+'''
+Input:
+0 0 0 2 6 0 7 0 1
+6 8 0 0 7 0 0 9 0
+1 9 0 0 0 4 5 0 0
+8 2 0 1 0 0 0 4 0
+0 0 4 6 0 2 9 0 0
+0 5 0 0 0 3 0 2 8
+0 0 9 3 0 0 0 7 4
+0 4 0 0 5 0 0 3 6
+7 0 3 0 1 8 0 0 0
+Output:
+4 3 5 2 6 9 7 8 1
+6 8 2 5 7 1 4 9 3
+1 9 7 8 3 4 5 6 2
+8 2 6 1 9 5 3 4 7
+3 7 4 6 8 2 9 1 5
+9 5 1 7 4 3 6 2 8
+5 1 9 3 2 6 8 7 4
+2 4 8 9 5 7 1 3 6
+7 6 3 4 1 8 2 5 9'''
+for i in range(9):
+	grid.append(list(map(int,input().split())))
+solve()
